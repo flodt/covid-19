3887c8a865aff66c7f20ffdc39438d6355aacf75
Florian Schmidt <fs.schmidt@tum.de>
2021-01-26 16:32:00 +0100

    update the vaccination URL to the new ZEIT source

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ window.addEventListener("load", function () {
     let agss = ["09778", "09162", "09179", "09762", "09777", "09188", "09178", "09175", "09772"];
     const URL_RKI = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=RS,AGS,GEN,EWZ,death_rate,cases,deaths,cases_per_100k,cases_per_population,BL,county,last_update,cases7_per_100k,recovered,cases7_bl,death7_bl,cases7_lk,death7_lk,cases7_per_100k_txt&returnGeometry=false&outSR=4326&f=json";
     const URL_ZEIT = "https://interactive.zeit.de/cronjobs/2020/corona/germany-dashboard-v2.json";
-    const URL_VACC = "https://interactive.zeit.de/cronjobs/2020/corona/impfzahlen.json";
+    const URL_VACC = "https://interactive.zeit.de/cronjobs/2020/corona/impfzahlenAutomatisch.json";
 
     //show loading indicator
     document.getElementById("preloader_container").style.display = "block";
